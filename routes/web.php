7034894fe1ebeba1bdd138101f956c4570b73458
Juliandavidbarrientospoli <julianjaramillo2011@gmail.com>
2023-10-31 00:20:51 -0500

    exprense reports laravel v1

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'App\Http\Controllers\HomeController@index');
+Route::get('/dashboard', 'App\Http\Controllers\DashboardController@index' );
+Route::resource('/expense_reports','App\Http\Controllers\ExpenseReportController');
